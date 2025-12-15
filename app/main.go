8455eccb898039a03360757a2cf785c88f8ada8a
muffin-5 <pratham.agarwal.cse22@iitbhu.ac.in>
2025-12-15 09:05:52 +0530

    REPL

--- app/main.go
+++ app/main.go
@@ -10,15 +10,18 @@ import (
 var _ = fmt.Print
 
 func main() {
-	// TODO: Uncomment the code below to pass the first stage
-	fmt.Print("$ ")
 
-	//Print invlid command
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Println("Err:", err)
-		return
+	for {
+		// TODO: Uncomment the code below to pass the first stage
+		fmt.Print("$ ")
+
+		//Print invlid command
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Println("Err:", err)
+			return
+		}
+
+		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
-
-	fmt.Println(command[:len(command)-1] + ": command not found")
 }
